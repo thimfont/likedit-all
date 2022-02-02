655a78e9bd0f865a0487c78f82669faf67d0e8c0
TMF <thiago.melo@outsmartdigital.com.br>
2022-02-01 23:45:18 -0300

    build: add libs for project.

---
+++ src/main/java/br/com/likedit/LikeditApplication.java
@@ -0,0 +1,13 @@
+package br.com.likedit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikeditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikeditApplication.class, args);
+	}
+
+}
