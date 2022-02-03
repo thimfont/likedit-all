9c04114cafb0262849a83103dbe0998b949fdbbb
TMF <thiago.melo@outsmartdigital.com.br>
2022-02-02 21:14:22 -0300

    feat: load script with data.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO user (id, name) VALUES (1, 'Thiago');
+INSERT INTO user (id, name) VALUES (2, 'Rose');
+INSERT INTO user (id, name) VALUES (3, 'Alyce');
+INSERT INTO user (id, name) VALUES (4, 'João');
+INSERT INTO user (id, name) VALUES (5, 'Nilce');
+INSERT INTO user (id, name) VALUES (6, 'Lucas');
+INSERT INTO user (id, name) VALUES (7, 'Dani');
+INSERT INTO user (id, name) VALUES (8, 'Isaque');
