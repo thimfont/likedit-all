48c0faddf815cc4790ee42468c781af83b837f0b
TMF <thiago.melo@outsmartdigital.com.br>
2022-02-08 22:41:33 -0300

    feat: change path for list users.

--- src/main/java/br/com/likedit/ui/HomeController.java
+++ src/main/java/br/com/likedit/ui/HomeController.java
@@ -2,22 +2,23 @@ package br.com.likedit.ui;
 
 import br.com.likedit.core.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class UsersController {
+@RequestMapping("/users")
+public class HomeController {
 
     private UserService userService;
 
     @Autowired
-    public UsersController(final UserService userService) {
+    public HomeController(final UserService userService) {
         this.userService = userService;
     }
 
-    @GetMapping("/users")
+    @GetMapping
     public String home(final Model model) {
         model.addAttribute("users", userService.all());
         return "users/users";
